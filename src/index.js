7c66506a30f59c9210328d193b798233a4d4b257
NghiaTT <tonghia2003@gmail.com>
2025-10-29 22:16:44 +0700

    Fix FrontEnd v2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
