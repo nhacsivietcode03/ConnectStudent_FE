4f05fd2ddf548fe48fe07bf907b62fdb07659b0f
NghiaTT <tonghia2003@gmail.com>
2025-10-29 22:13:45 +0700

    Fix FrontEnd

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,57 +1,86 @@
-import React from 'react';
-import {
-    MDBBtn,
-    MDBContainer,
-    MDBCard,
-    MDBCardBody,
-    MDBCardImage,
-    MDBRow,
-    MDBCol,
-    MDBIcon,
-    MDBInput
-}
-    from 'mdb-react-ui-kit';
 
 function LoginScreen() {
     return (
-        <MDBContainer className="my-5">
+        <div className="container my-5">
+            <div className="card">
+                <div className="row g-0">
 
-            <MDBCard>
-                <MDBRow className='g-0'>
+                    <div className="col-md-6">
+                        <img
+                            src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
+                            alt="login form"
+                            className="img-fluid rounded-start w-100"
+                        />
+                    </div>
 
-                    <MDBCol md='6'>
-                        <MDBCardImage src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp' alt="login form" className='rounded-start w-100' />
-                    </MDBCol>
-
-                    <MDBCol md='6'>
-                        <MDBCardBody className='d-flex flex-column'>
-
-                            <div className='d-flex flex-row mt-2'>
-                                <MDBIcon fas icon="cubes fa-3x me-3" style={{ color: '#ff6219' }} />
+                    <div className="col-md-6 d-flex align-items-center">
+                        <div className="card-body p-4 p-lg-5 text-black">
+                            <div className="d-flex flex-row mt-2 align-items-center">
+                                <i
+                                    className="fas fa-cubes fa-3x me-3"
+                                    style={{ color: "#ff6219" }}
+                                ></i>
                                 <span className="h1 fw-bold mb-0">Logo</span>
                             </div>
 
-                            <h5 className="fw-normal my-4 pb-3" style={{ letterSpacing: '1px' }}>Sign into your account</h5>
+                            <h5 className="fw-normal my-4 pb-3" style={{ letterSpacing: "1px" }}>
+                                Sign into your account
+                            </h5>
 
-                            <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" />
-                            <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg" />
-
-                            <MDBBtn className="mb-4 px-5" color='dark' size='lg'>Login</MDBBtn>
-                            <a className="small text-muted" href="#!">Forgot password?</a>
-                            <p className="mb-5 pb-lg-2" style={{ color: '#393f81' }}>Don't have an account? <a href="#!" style={{ color: '#393f81' }}>Register here</a></p>
-
-                            <div className='d-flex flex-row justify-content-start'>
-                                <a href="#!" className="small text-muted me-1">Terms of use.</a>
-                                <a href="#!" className="small text-muted">Privacy policy</a>
+                            <div className="form-outline mb-4">
+                                <label className="form-label" htmlFor="email">
+                                    Email address
+                                </label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    className="form-control form-control-lg"
+                                    placeholder="Enter your email"
+                                />
                             </div>
 
-                        </MDBCardBody>
-                    </MDBCol>
+                            <div className="form-outline mb-4">
+                                <label className="form-label" htmlFor="password">
+                                    Password
+                                </label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    className="form-control form-control-lg"
+                                    placeholder="Enter your password"
+                                />
+                            </div>
 
-                </MDBRow>
-            </MDBCard>
+                            <button
+                                className="btn btn-dark btn-lg btn-block mb-4 px-5"
+                                type="button"
+                            >
+                                Login
+                            </button>
 
-        </MDBContainer>
+                            <a className="small text-muted d-block" href="#!">
+                                Forgot password?
+                            </a>
+                            <p className="mb-5 pb-lg-2" style={{ color: "#393f81" }}>
+                                Don't have an account?{" "}
+                                <a href="#!" style={{ color: "#393f81" }}>
+                                    Register here
+                                </a>
+                            </p>
+
+                            <div className="d-flex flex-row justify-content-start">
+                                <a href="#!" className="small text-muted me-1">
+                                    Terms of use.
+                                </a>
+                                <a href="#!" className="small text-muted">
+                                    Privacy policy
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
